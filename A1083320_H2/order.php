3d1f055736562b92f7ea80c846eecfd378f3e852
Den0601 <61945850+Den0601@users.noreply.github.com>
2020-03-25 20:24:26 +0800

    Add files via upload

---
+++ A1083320_H2/order.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<title>A1083320 訂票頁面</title>
+</head>
+<body bgcolor="BBFFFF">
+	
+<center>
+	
+<h1><font face="標楷體">歡迎蒞臨</h1>
+<h2>歡迎蒞臨</h2>
+<h3>歡迎蒞臨</h3>
+<h4>歡迎蒞臨</h4>
+<h5>歡迎蒞臨</h5>
+<h6>歡迎蒞臨</h6>
+</font>
+<a href="price.html">
+<img src="https://upload.cc/i1/2020/03/11/uCbne0.png">
+</a>
+</center>
+<form action="test.php" method="POST">
+<p align="middle">
+<font color="#B22222">姓名<input type="text" name="name"required><br/>
+學號<input type="text" name="number"required></font><br/>
+<font color="0000CD">性別<br/>
+Male<input type="radio"  value="Male"  name="gender"required>
+Female<input type="radio" value="Female" name="gender"required></font><br/>
+<font color="#6A5ACD">票數<br/>
+全票<input type="text" name="adult"required><br/>
+敬老票<input type="text" name="old"required><br/>
+愛心票<input type="text" name="heart"required></font>
+<br/>
+<font color="#43CD80">起訖站<br/>
+起點站<input type="text" name="station1" required><br/>
+終點站<input type="text" name="station2"required><br/>
+
+座位喜好
+靠窗<input type="radio" name="seat"required>
+靠走道<input type="radio" name="seat"required><br/>
+<br/>連絡電話<input type="text" name="telephone"required><br/>
+<br/>信箱<input type="email" name="mail"required><br/>
+<br/>備註<input type="text" name="other"> <br/></font>
+
+  <br/><input type="submit" value="提交">
+  
+　<input type="reset" value="重新輸入">
+
+</p>
+</form>
+
+
+
+
+
+
+
+</body>	
+</html>
---
+++ A1083320_H2/test.php
@@ -0,0 +1,45 @@
+<?php
+
+$姓名=$_POST["name"];
+$學號=$_POST["number"];
+$性別=$_POST["gender"];
+$全票=$_POST["adult"];
+$敬老票=$_POST["old"];
+$愛心票=$_POST["heart"];
+$起點站=$_POST["station1"];
+$終點站=$_POST["station2"];
+$座位喜好=$_POST["seat"];
+$連絡電話=$_POST["telephone"];
+$信箱=$_POST["mail"];
+$備註=$_POST["other"];
+
+
+
+echo "姓名:".$姓名."<br/>";
+echo "學號:".$學號."<br/>";
+echo "性別:".$性別."<br/>";
+echo "全票:".$全票."<br/>";
+echo "敬老票:".$敬老票."<br/>";
+echo "愛心票:".$愛心票."<br/>";
+echo "起點站:".$起點站."<br/>";
+echo "終點站:".$終點站."<br/>";
+echo "座位喜好:".$座位喜好."<br/>";
+echo "連絡電話:".$連絡電話."<br/>";
+echo "信箱:".$信箱."<br/>";
+echo "備註:".$備註."<br/>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
